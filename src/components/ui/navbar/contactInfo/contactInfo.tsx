baaaef11bffa1104bfdf6f508101fd95b7f9434c
Francesco <sennozeta@gmail.com>
2023-10-05 10:06:20 +0100

    Fixed Contact Info Box

--- src/components/ui/navbar/contactInfo/contactInfo.tsx
+++ src/components/ui/navbar/contactInfo/contactInfo.tsx
@@ -4,50 +4,62 @@ import { Menu, Transition } from '@headlessui/react';
 export const ContactInfo = () => {
   return (
     <div className='mt-10 md:m-0 flex gap-10 border-2 border-white-60-opacity px-5 py-2 rounded justify-center'>
-      <button className='flex-none w-30 text-center'>+44 192033894</button>
+      <div className='flex-none w-30 text-center'>
+        <a href='tel:000000-000'>+44 000000 0000</a>
+      </div>
       <div className='w-[2px] bg-white-60-opacity'></div>
+
       <Menu as='div' className='relative inline-block text-left flex-none w-32'>
-        <Menu.Button className='w-full text-center transition-colors duration-200 focus:outline-none focus:ring'>
-          English
-        </Menu.Button>
-        <Transition
-          as={Fragment}
-          enter='transition ease-out duration-100'
-          enterFrom='transform opacity-0 scale-95'
-          enterTo='transform opacity-100 scale-100'
-          leave='transition ease-in duration-75'
-          leaveFrom='transform opacity-100 scale-100'
-          leaveTo='transform opacity-0 scale-95'
-        >
-          <Menu.Items className='absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none z-10'>
-            <div className='py-1'>
-              <Menu.Item>
-                {({ active }) => (
-                  <a
-                    className={`${
-                      active ? 'bg-gray-100' : ''
-                    } block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100`}
-                    href='#'
-                  >
-                    English
-                  </a>
-                )}
-              </Menu.Item>
-              <Menu.Item>
-                {({ active }) => (
-                  <a
-                    className={`${
-                      active ? 'bg-gray-100' : ''
-                    } block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100`}
-                    href='#'
-                  >
-                    Italian
-                  </a>
-                )}
-              </Menu.Item>
-            </div>
-          </Menu.Items>
-        </Transition>
+        {({ open }) => (
+          <>
+            <Menu.Button
+              className={`${
+                open &&
+                'transition-all bg-white rounded rounded-b-none text-black p-0 m-0'
+              } w-full text-center transition-colors duration-200 focus:outline-none focus:ring`}
+            >
+              English
+            </Menu.Button>
+            <Transition
+              as={Fragment}
+              enter='transition ease-out duration-100'
+              enterFrom='transform opacity-0 scale-95'
+              enterTo='transform opacity-100 scale-100'
+              leave='transition ease-in duration-75'
+              leaveFrom='transform opacity-100 scale-100'
+              leaveTo='transform opacity-0 scale-95'
+            >
+              <Menu.Items className='absolute right-0 w-full rounded rounded-t-none shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none z-10'>
+                <div className='py-1'>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <a
+                        className={`${
+                          active ? 'bg-gray-100' : ''
+                        } block px-4 py-2 text-sm text-black hover:text-white hover:bg-red-600`}
+                        href='#'
+                      >
+                        English
+                      </a>
+                    )}
+                  </Menu.Item>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <a
+                        className={`${
+                          active ? 'bg-gray-100' : ''
+                        }  block px-4 py-2 text-sm text-black hover:text-white hover:bg-red-600`}
+                        href='#'
+                      >
+                        Italian
+                      </a>
+                    )}
+                  </Menu.Item>
+                </div>
+              </Menu.Items>
+            </Transition>
+          </>
+        )}
       </Menu>
     </div>
   );
--- src/components/ui/navbar/navbar.tsx
+++ src/components/ui/navbar/navbar.tsx
@@ -59,22 +59,34 @@ export const Navbar = () => {
                 className={`flex flex-col gap-2 md:flex-row justify-evenly flex-auto uppercase`}
               >
                 <li>
-                  <a className='hover:underline' href='#'>
+                  <a
+                    className=' border-transparent transition-all hover:border-b-4 hover:border-red-500'
+                    href='#'
+                  >
                     Cruises
                   </a>
                 </li>
                 <li>
-                  <a className='hover:underline' href='#'>
+                  <a
+                    className=' border-transparent transition-all hover:border-b-4 hover:border-red-500'
+                    href='#'
+                  >
                     How to find us
                   </a>
                 </li>
                 <li>
-                  <a className='hover:underline' href='#'>
+                  <a
+                    className=' border-transparent transition-all hover:border-b-4 hover:border-red-500'
+                    href='#'
+                  >
                     Accomodation
                   </a>
                 </li>
                 <li>
-                  <a className='hover:underline' href='#'>
+                  <a
+                    className=' border-transparent transition-all hover:border-b-4 hover:border-red-500'
+                    href='#'
+                  >
                     Cafe
                   </a>
                 </li>
